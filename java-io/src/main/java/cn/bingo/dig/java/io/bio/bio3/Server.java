45b1c0c03342b1c24919f5102cbb1e2123929347
Bingo Yang <yangyingbin0107@163.com>
2018-12-22 23:31:25 +0800

    add a acceptor on server
    Change-Id: Ideba7dfd3e3a05d5763dd47879edc87fb890def3
    Signed-off-by: Bingo Yang <yangyingbin0107@163.com>

--- java-io/src/main/java/cn/bingo/dig/java/io/bio/bio3/Server.java
+++ java-io/src/main/java/cn/bingo/dig/java/io/bio/bio3/Server.java
@@ -5,6 +5,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +16,8 @@ public class Server {
   private int port = 8088;
   private ServerSocket server;
   private ExecutorService executorService = Executors.newFixedThreadPool(10);
+  private Queue<String> cache = new LinkedList<String>();
+  private boolean running = true;
 
   public static void main(String[] args) {
     Server server = new Server();
@@ -26,31 +30,80 @@ public class Server {
       log.debug("Server start port " + port);
       while (true) {
         Socket socket = server.accept();
-        requestHandler(socket);
+        executorService.execute(clientHandler(socket));
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
-  private void requestHandler(Socket socket) {
-    byte[] buf = new byte[1024];
-    try (DataInputStream dis = new DataInputStream(socket.getInputStream());
-        DataOutputStream dos = new DataOutputStream(socket.getOutputStream())) {
+  private Runnable clientHandler(Socket socket) {
+    return () -> {
+      log.debug("the clientHandler[id={}] is start", Thread.currentThread().getId());
+      executorService.execute(socketRead(socket));
+      executorService.execute(socketwrite(socket));
+    };
+  }
 
-      int len = dis.read(buf);
-      while (len != -1) {
-        String line = new String(buf,0,len);
-        log.debug(line);
-        dos.write(buf,0,len);
-        dos.flush();
-        len = dis.read(buf);
+  private Runnable socketwrite(Socket socket) {
+    return () -> {
+      log.debug("the socketwrite[id={}] is start", Thread.currentThread().getId());
+      try (DataInputStream dis = new DataInputStream(socket.getInputStream())) {
+        byte[] buf = new byte[1024 * 2];
+        int len = -1;
+        String line = null;
+        synchronized (cache) {
+          while (running) {
+            len = dis.read(buf);
+            if (len != -1 && cache.size() <= 100) {
+              line = new String(buf, 0, len);
+              log.debug("string read from client[threadId:{}]: {}", Thread.currentThread().getId(),
+                  line);
+              cache.add(line);
+            }
+            if (cache.size() > 0) {
+              cache.notifyAll();
+              cache.wait();
+            }
+          }
+        }
+
+      } catch (IOException e) {
+        e.printStackTrace();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
       }
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
 
+    };
+  }
+
+  private Runnable socketRead(Socket socket) {
+    return () -> {
+      log.debug("the socketRead[id={}] is start", Thread.currentThread().getId());
+      try (DataOutputStream dos = new DataOutputStream(socket.getOutputStream())) {
+        String line = null;
+        while (running) {
+          synchronized (cache) {
+            line = cache.poll();
+            if (line == null) {
+              log.debug("cache is empty,the current thread[id={}] is release the lock",
+                  Thread.currentThread().getId());
+              cache.notifyAll();
+              cache.wait();
+            } else {
+              log.debug("string write to client[threadId:{}]: {}", Thread.currentThread().getId(),
+                  line);
+              dos.write(line.getBytes());
+              dos.flush();
+            }
+          }
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    };
   }
 
 }
