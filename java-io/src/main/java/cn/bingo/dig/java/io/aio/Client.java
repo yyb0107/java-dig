d4810a63f5178a07b29457a07a4464c706566f61
Bingo Yang <yangyingbin0107@163.com>
2019-01-05 23:23:22 +0800

    asynchronousServerSocketChannel preparation
    Change-Id: I44f1c955f8dd297261fcebfbf6be882ebfd6f9a5
    Signed-off-by: Bingo Yang <yangyingbin0107@163.com>

---
+++ java-io/src/main/java/cn/bingo/dig/java/io/aio/Client.java
@@ -0,0 +1,67 @@
+package cn.bingo.dig.java.io.aio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Client {
+  private int port = 8088;
+  private boolean running = true;
+
+  public static void main(String[] args) {
+    Client client = new Client();
+    client.connect();
+  }
+
+  public void connect() {
+    InetSocketAddress address = new InetSocketAddress("127.0.0.1", port);
+    try (SocketChannel socketChannel = SocketChannel.open();) {
+      socketChannel.connect(address);
+      log.debug("client connected server{} succeed!", address);
+      socketChannel.configureBlocking(false);
+      Selector selector = Selector.open();
+      socketChannel.register(selector, SelectionKey.OP_WRITE);
+      while (running) {
+        selector.select();
+        Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+        ByteBuffer buf = ByteBuffer.allocate(1024);
+        while (it.hasNext()) {
+          SelectionKey key = it.next();
+          it.remove();
+          if (key.isValid()) {
+            if (key.isWritable()) {
+              SocketChannel channel = (SocketChannel) key.channel();
+              for (int i = 0; i < 6; i++) {
+                channel.write(ByteBuffer.wrap(new String("Hello World " + i).getBytes()));
+              }
+              log.debug("client send msg succeed!");
+              channel.register(selector, SelectionKey.OP_READ);
+            } else if (key.isReadable()) {
+              buf.clear();
+              SocketChannel channel = (SocketChannel) key.channel();
+              channel.read(buf);
+              buf.flip();
+              log.debug("response from server {}", new String(buf.array()));
+              // channel.close();
+              // channel.register(selector, SelectionKey.OP_WRITE);
+            }
+          }
+        }
+      }
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+  }
+
+  public void shutdown() {
+    running = false;
+  }
+}
--- java-io/src/main/java/cn/bingo/dig/java/io/aio/Server.java
+++ java-io/src/main/java/cn/bingo/dig/java/io/aio/Server.java
@@ -1,9 +1,65 @@
 package cn.bingo.dig.java.io.aio;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Server {
+
+
+
   private final int PORT = 8088;
 
   public void start() {
+    try (AsynchronousServerSocketChannel serverChannel = AsynchronousServerSocketChannel.open();) {
+      InetSocketAddress address = new InetSocketAddress(PORT);
+      serverChannel.bind(address);
+      serverChannel.accept(this, new AcceptHandler());
+      log.debug("Server started up... port[{}]", PORT);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 
+
+
+  public static void main(String[] args) {
+    Server server = new Server();
+    server.start();
+
+  }
+}
+
+
+@Slf4j
+class AcceptHandler implements CompletionHandler<AsynchronousSocketChannel, Server> {
+  @Override
+  public void completed(AsynchronousSocketChannel socketChannel, Server server) {
+    // super.completed(socketChannel, server);
+    try {
+      ByteBuffer dst = ByteBuffer.allocate(1024);
+      Future<Integer> future = socketChannel.read(dst);
+      int len = future.get();
+      dst.flip();
+      log.debug("read len: {}", len);
+      String request = new String(dst.array(), 0, len);
+      log.debug("String from client {}", request);
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void failed(Throwable arg0, Server arg1) {
+    // TODO Auto-generated method stub
+    log.error("failed... ");
   }
 }
