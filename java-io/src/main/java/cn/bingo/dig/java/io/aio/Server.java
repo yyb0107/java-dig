e5d7260de01ca54872888d312353baa2d66447e5
Bingo Yang <yangyingbin0107@163.com>
2019-01-06 23:58:19 +0800

    AsynchronousCloseException occurred when server started up
    Change-Id: Id2f9819bc6ade128915fd25a0b794765c76bd385
    Signed-off-by: Bingo Yang <yangyingbin0107@163.com>

--- java-io/src/main/java/cn/bingo/dig/java/io/aio/Server.java
+++ java-io/src/main/java/cn/bingo/dig/java/io/aio/Server.java
@@ -12,17 +12,32 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Server {
-
+  private AsynchronousServerSocketChannel serverChannel;
 
 
   private final int PORT = 8088;
 
   public void start() {
     try (AsynchronousServerSocketChannel serverChannel = AsynchronousServerSocketChannel.open();) {
+      this.serverChannel = serverChannel;
       InetSocketAddress address = new InetSocketAddress(PORT);
-      serverChannel.bind(address);
-      serverChannel.accept(this, new AcceptHandler());
+      this.serverChannel.bind(address, 100);
+      this.serverChannel.accept(this, new AcceptHandler());
       log.debug("Server started up... port[{}]", PORT);
+      Thread t = new Thread(new Runnable() {
+        @Override
+        public void run() {
+          while (true) {
+            // System.out.println("运行中...");
+            try {
+              Thread.sleep(1000);
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+          }
+        }
+      });
+      t.start();
     } catch (IOException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
@@ -30,6 +45,9 @@ public class Server {
   }
 
 
+  public AsynchronousServerSocketChannel getServerChannel() {
+    return serverChannel;
+  }
 
   public static void main(String[] args) {
     Server server = new Server();
@@ -45,21 +63,30 @@ class AcceptHandler implements CompletionHandler<AsynchronousSocketChannel, Serv
   public void completed(AsynchronousSocketChannel socketChannel, Server server) {
     // super.completed(socketChannel, server);
     try {
-      ByteBuffer dst = ByteBuffer.allocate(1024);
-      Future<Integer> future = socketChannel.read(dst);
-      int len = future.get();
-      dst.flip();
-      log.debug("read len: {}", len);
-      String request = new String(dst.array(), 0, len);
-      log.debug("String from client {}", request);
-    } catch (InterruptedException | ExecutionException e) {
-      e.printStackTrace();
+//      ByteBuffer dst = ByteBuffer.allocate(1024);
+//      Future<Integer> future = socketChannel.read(dst);
+//      int len = future.get();
+//      dst.flip();
+//      log.debug("read len: {}", len);
+//      String request = new String(dst.array(), 0, len);
+      log.debug("String from client {}", server);
+    } finally {
+      server.getServerChannel().accept(server, this);
     }
   }
 
   @Override
-  public void failed(Throwable arg0, Server arg1) {
+  public void failed(Throwable exception, Server server) {
     // TODO Auto-generated method stub
-    log.error("failed... ");
+    try {
+      log.error("failed... ");
+      exception.printStackTrace();
+      log.error("{}", server.getServerChannel());
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } finally {
+//      server.getServerChannel().accept(server, this);
+    }
   }
 }
